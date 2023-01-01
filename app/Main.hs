b8fa29a6df2a688199bb90d60fad6f860c346b1f
Travis Cardwell <travis.cardwell@extrema.is>
2023-01-01 09:32:30 +0900

    Update copyright date

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 -- |
 -- Module      : Main
 -- Description : lsupg: list items in a container that can be upgraded
--- Copyright   : Copyright (c) 2021-2022 Travis Cardwell
+-- Copyright   : Copyright (c) 2021-2023 Travis Cardwell
 -- License     : MIT
 --
 -- See the README for details.
--- src/LsUpg/Component/Pacman.hs
+++ src/LsUpg/Component/Pacman.hs
@@ -2,7 +2,7 @@
 -- |
 -- Module      : LsUpg.Component.Pacman
 -- Description : pacman component
--- Copyright   : Copyright (c) 2021-2022 Travis Cardwell
+-- Copyright   : Copyright (c) 2021-2023 Travis Cardwell
 -- License     : MIT
 ------------------------------------------------------------------------------
 
